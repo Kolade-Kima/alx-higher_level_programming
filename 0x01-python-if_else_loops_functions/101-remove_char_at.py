72531d558f119feeae279aabf2d116538f96d265
Kolade-Kima <afolabikolade11@gmail.com>
2022-08-24 05:00:02 +0000

    pushing 14 and 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-        if (n > len(str) or n < 0):
-                    return str
-        else:
-                    listStr = list(str)
-                            listStr.pop(n)
-                                    str = "".join(listStr)
-                                            return str
-                                        
+    if (n > len(str) or n < 0):
+        return str
+    else:
+        listStr = list(str)
+        listStr.pop(n)
+        str = "".join(listStr)
+        return str
